7e18f8178804bb424ac7ee2f798929d32e6ad6ac
Chris Rodgers <devdigression@gmail.com>
2017-10-31 18:50:58 -0400

    added jobs progression stats

--- js/app.js
+++ js/app.js
@@ -12,6 +12,7 @@ $(function() {
 		event.preventDefault();
 		userCareerQuery = $('#loc-query').val();
 		$('#loc-query').val("");
+		$('#home-page-header').addClass('no-display');
 		$('#home-page').addClass('no-display');
 		$('#loc-page').removeClass('no-display');
 		retrieveJobStats(displayLocData);
@@ -24,6 +25,7 @@ $(function() {
 		userCurrentCareer = $('#career-query').val();
 		console.log(userCurrentCareer);
 		$('#career-query').val("");
+		$('#home-page-header').addClass('no-display');
 		$('#home-page').addClass('no-display');
 		$('#progression-page').removeClass('no-display');
 		retrieveJobProg(displayCareerProgression);
@@ -103,7 +105,9 @@ function retrieveJobProg (callback) {
 }
 
 function displayLocData (results) {
-console.log(results);
+	$('#top-states h3').text(`Top 5 States for ${userCareerQuery}`);
+	$('#top-cities h3').text(`Top 5 Cities for ${userCareerQuery}`);
+
 	let cities = results.response.cities;
 	const citiesTopFive = [];
 	for (let i = 0; i < 5; i++) {
@@ -111,11 +115,17 @@ console.log(results);
 	}
 	const citiesList = citiesTopFive.map((item, index) => renderCitiesResults(item));
 		$('#cities-list').html(citiesList);
-
-	const statesTopFive = [];
-	for (let i = 0; i < 5; i++) {
-		statesTopFive.push(addStateJobs(results));
-	}
+	
+	const stateResults = addStateJobs(results);
+	let topFiveStates = Object.keys(stateResults).map(function(state) {
+  		return { 
+  			stateName: state, 
+  			jobs: this[state] };
+		}, stateResults);
+		topFiveStates.sort(function(state1, state2) { 
+		return state2.jobs - state1.jobs; 
+	});
+	const statesTopFive = topFiveStates.slice(0, 5);
 	const statesList = statesTopFive.map((item, index) => renderStatesResults(item));
 		$('#states-list').html(statesList);
 }
@@ -134,6 +144,8 @@ function renderStatesResults(state) {
 
 function displayRelatedCareers (results) {
 	console.log(results);
+
+	$('#related-careers h3').text("Jobs related to " + userCareerQuery);
 	let careers = results.response.jobTitles;
 	
 	const careersList = careers.map((item, index) => renderRelatedCareers(item));
@@ -174,13 +186,14 @@ function addStateJobs(results) {
   	}
   }
 
+  	console.log(stateCount);
   	// uStates.draw('#statesvg', calculateSampleData(stateAbb), tooltipHtml);
    	return stateCount;
 }
 
 function displayCareerProgression (results) {
 	console.log(results);
-	$('#progression-page h2').text(`Check out these career options related to \"${capitalize(userCurrentCareer)}\"`);
+	$('#progression-page h2').text(`Check out these career options related to ${capitalize(userCurrentCareer)}`);
 	let jobs = results.response.results;
 	const jobsProg = [];
 	for (let i = 0; i < 5; i++) {
@@ -193,7 +206,12 @@ function displayCareerProgression (results) {
 
 function renderJobProg(job) {
 	return `
-		<li>${job.nextJobTitle}</li>
+		<div class="related-job">
+		<h3>${capitalize(job.nextJobTitle)}</h3>
+		<li>Frequency of ${userCurrentCareer}s taking this job: ${Math.round(job.frequencyPercent*100)/100}%</li>
+		<li>Jobs available: ${job.nationalJobCount}</li>
+		<li>Median Salary: $${job.medianSalary}</li>
+		</div>
 		`
 }
 
